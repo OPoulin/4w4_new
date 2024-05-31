ea21c5aec844a777c13ffc42cca1001ec5a483cf
Olivier Poulin <456crdv@gmail.com>
2024-05-31 14:28:23 -0400

    Ajout du slideshow de certains pays et début mise en page de la page

--- wp-content/plugins/pays/pays.php
+++ wp-content/plugins/pays/pays.php
@@ -5,3 +5,12 @@ Description: Affiche les destinations de différents pays
 Version: 1.0
 Author: Olivier Poulin
 */
+
+
+
+wp_enqueue_script(  'em_plugin_voyage_js',
+                    plugin_dir_url(__FILE__) ."js/voyage.js",
+                    array(),
+                    $version_js,
+                    true);
+?>
